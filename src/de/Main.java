296aee57279acddfd2fa23541601c8735f39a6f0
Florian2102 <Florian@83ad4bda.funky.hs-osnabrueck.de>
2014-12-06 11:50:22 +0100

    while weg

--- src/de/Main.java
+++ src/de/Main.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Main {
-	
+
 	public static BufferedReader br = new BufferedReader(new InputStreamReader(
 			System.in));
 
@@ -19,23 +19,17 @@ public class Main {
 		Knoten e = new Knoten("E", 0, null);
 		Knoten f = new Knoten("F", 0, null);
 
-		a.adjacencies = new Kante[] {new Kante(b, 4),
-				new Kante(c, 2),
-				new Kante(d, 5)};
-		b.adjacencies = new Kante[] {new Kante(a, 4),
-				new Kante(e, 6),
-				new Kante(f, 4)};
-		c.adjacencies = new Kante[] {new Kante(a, 2),
-				new Kante(d, 5)};
-		d.adjacencies = new Kante[] {new Kante(a, 5),
-				new Kante(e, 5),
-				new Kante(f, 3)};
-		e.adjacencies = new Kante[] {new Kante(b, 6),
-				new Kante(d, 5),
-				new Kante(f, 1)};
-		f.adjacencies = new Kante[] {new Kante(b, 4),
-				new Kante(d, 3),
-				new Kante(e, 1)};
+		a.adjacencies = new Kante[] { new Kante(b, 4), new Kante(c, 2),
+				new Kante(d, 5) };
+		b.adjacencies = new Kante[] { new Kante(a, 4), new Kante(e, 6),
+				new Kante(f, 4) };
+		c.adjacencies = new Kante[] { new Kante(a, 2), new Kante(d, 5) };
+		d.adjacencies = new Kante[] { new Kante(a, 5), new Kante(e, 5),
+				new Kante(f, 3) };
+		e.adjacencies = new Kante[] { new Kante(b, 6), new Kante(d, 5),
+				new Kante(f, 1) };
+		f.adjacencies = new Kante[] { new Kante(b, 4), new Kante(d, 3),
+				new Kante(e, 1) };
 
 		ArrayList<Knoten> besucht = new ArrayList<Knoten>();
 		ArrayList<Knoten> rand = new ArrayList<Knoten>();
@@ -44,16 +38,16 @@ public class Main {
 		rand.add(a);
 
 		if (rand.contains(a)) {
-			
+
 			unbekannt.add(b);
 			unbekannt.add(c);
 			unbekannt.add(d);
 			unbekannt.add(e);
 			unbekannt.add(f);
 		}
-		
+
 		if (rand.contains(b)) {
-			
+
 			unbekannt.add(a);
 			unbekannt.add(c);
 			unbekannt.add(d);
@@ -62,7 +56,7 @@ public class Main {
 		}
 
 		if (rand.contains(c)) {
-	
+
 			unbekannt.add(a);
 			unbekannt.add(b);
 			unbekannt.add(d);
@@ -71,7 +65,7 @@ public class Main {
 		}
 
 		if (rand.contains(d)) {
-	
+
 			unbekannt.add(a);
 			unbekannt.add(b);
 			unbekannt.add(c);
@@ -80,7 +74,7 @@ public class Main {
 		}
 
 		if (rand.contains(e)) {
-	
+
 			unbekannt.add(a);
 			unbekannt.add(b);
 			unbekannt.add(c);
@@ -89,21 +83,19 @@ public class Main {
 		}
 
 		if (rand.contains(f)) {
-	
+
 			unbekannt.add(a);
 			unbekannt.add(b);
 			unbekannt.add(c);
 			unbekannt.add(d);
 			unbekannt.add(e);
 		}
-		
 
 		Knoten aktiv = null;
 		Knoten ziel = e;
 
-
 		do {
-			
+
 			// aktiv entnehmen mit kleinstem Gewicht aus rand
 			Collections.sort(rand);
 			aktiv = rand.get(0);
@@ -115,21 +107,23 @@ public class Main {
 				System.out.println("Wir sind am Ziel.");
 			}
 
-			// ansonsten füge aktiv zu besucht hinzu
+			// ansonsten fï¿½ge aktiv zu besucht hinzu
 			else {
 
 				besucht.add(aktiv);
 			}
 
-			// betrachte jeden von a aus direkt erreichbaren nachbarknoten n (aktiv.adjacencies[i])
+			// betrachte jeden von a aus direkt erreichbaren nachbarknoten n
+			// (aktiv.adjacencies[i])
 			for (int i = 0; i < aktiv.adjacencies.length; i++) {
 
-				// sein gesamtgewicht (gewicht) = gewicht aktiv + 
+				// sein gesamtgewicht (gewicht) = gewicht aktiv +
 				int gewicht = aktiv.adjacencies[i].laenge + aktiv.gewicht;
 
 				if (rand.contains(aktiv.adjacencies[i].knoten)) {
 
-					if (gewicht < aktiv.adjacencies[i].knoten.gewicht || aktiv.adjacencies[i].knoten.gewicht == 0) {
+					if (gewicht < aktiv.adjacencies[i].knoten.gewicht
+							|| aktiv.adjacencies[i].knoten.gewicht == 0) {
 						aktiv.adjacencies[i].knoten.gewicht = gewicht;
 						aktiv.adjacencies[i].knoten.vorgaenger = aktiv;
 					}
@@ -146,8 +140,7 @@ public class Main {
 			}
 
 		} while (aktiv != ziel);
-		
-		while ()
+
 		Knoten ergebnis = null;
 		Knoten ergebnis2 = null;
 		System.out.println(ziel.name);
